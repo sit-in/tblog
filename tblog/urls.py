e222c40bbff35f13ae996b788f3795dad4f6bed3
sitin <pptsuse@gmail.com>
2015-12-05 10:11:00 +0800

    deleted article_filter

--- tblog/urls.py
+++ tblog/urls.py
@@ -4,6 +4,6 @@ from tblog.index import IndexView, IndexDetailView
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^(?P<alias_name>[\w-]+)\.html$', IndexDetailView.as_view(), name='article-detail'),
-    url(r'^admin/', include('mongo_admin.articles.urls')),
+    url(r'^tadmin/', include('mongo_admin.articles.urls')),
     url(r'^accounts/', include('mongo_admin.accounts.urls')),
 ]
