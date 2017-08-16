425c90070c09010b502d320258742555e99e4696
sitin <pptsuse@gmail.com>
2017-08-16 23:57:43 +0800

    Fix: change debug ==> False

--- tblog/settings.py
+++ tblog/settings.py
@@ -7,24 +7,19 @@ import pymongo
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2)xd63epwoli9n6=e7e5rfe^42_*j#qg0qma17i^&i6(a*2)qs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 404 page need debug = False
 if DEBUG:
     DOMAIN = 'http://localhost:3000'
-    DB_NAME = 'tblog'
 else:
-    DOMAIN = 'http://www.ipengtao.com'
-    DB_NAME = 'tblog'
+    DOMAIN = 'http://blog.ipengtao.com'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sitin.me', 'ipengtao.com', 'www.ipengtao.com']
+DB_NAME = 'tblog'
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ipengtao.com', 'www.ipengtao.com']
 
 # mongoengine settings
 _MONGODB_HOST = '127.0.0.1:27017'
@@ -82,21 +77,12 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 WSGI_APPLICATION = 'tblog.wsgi.application'
 
-# TEMPLATE_LOADERS = (
-    # 'django_jinja.loaders.AppLoader',
-    # 'django_jinja.loaders.FileSystemLoader',
-# )
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.dummy'
     }
 }
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'zh-CN'
 
@@ -107,11 +93,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/themes/static/'
 
 STATICFILES_DIRS = (
@@ -119,5 +100,3 @@ STATICFILES_DIRS = (
 )
 
 MONGO_POOL = pymongo.mongo_client.MongoClient('localhost', 27017)
-
-# MARKDOWN_DEUX_HELP_URL = "/help/markdown/"
